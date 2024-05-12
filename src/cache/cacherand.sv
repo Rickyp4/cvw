b922c3d41fe36ad151e9bd6f7baf67e98ee06d38
Rickyp4 <richard.w.powers@okstate.edu>
2024-05-11 23:14:39 -0500

    regression sucks

--- src/cache/cacherand.sv
+++ src/cache/cacherand.sv
@@ -31,7 +31,7 @@ module cacherand
     // LSFR Module
     flopen #(LOGNUMWAYS+2) LSFReg (clk, reset, 1'b1, next, val, curr)
 
-    assign next[LOGNUMWAYS:0] = curr[LOGNUMWAYS+1:1];
+    next[LOGNUMWAYS:0] = curr[LOGNUMWAYS+1:1];
     if ((LOGNUMWAYS+2) == 3) begin
         assign next[2] = curr[2] ^ curr[0];
     end else if ((LOGNUMWAYS+2) == 4) begin
